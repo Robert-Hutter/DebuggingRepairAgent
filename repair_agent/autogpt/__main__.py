c4b215c2fa2b347f2b7a0815e5affc6abe885fc2
Robert Hutter <spam@robert-hutter.de>
2025-09-05 08:54:45 +0000

    Add tracing to debugger API calls using tracer script.

--- repair_agent/autogpt/__main__.py
+++ repair_agent/autogpt/__main__.py
@@ -1,5 +1,22 @@
 """Auto-GPT: A GPT powered AI Assistant"""
 import autogpt.app.cli
+from autogpt.tracer import Tracer
+import autogpt.debugger
+from autogpt.debugger.debugger_client import AgentDebugger
+from datetime import datetime
 
 if __name__ == "__main__":
-    autogpt.app.cli.main()
+    print('Starting trace...')
+    
+    tracer = Tracer(run_id=datetime.now().strftime("run_%b_%d_%H-%M-%S"))
+    tracer.instrument_class(AgentDebugger, name_prefix="AgentDebugger", library="autogpt.debugger")
+    
+    try:
+        with tracer.program("entire_run", scenario="baseline"):
+            autogpt.app.cli.main()
+    except KeyboardInterrupt:
+        raise
+    finally:
+        print('Saving trace...')
+        tracer.restore()
+        tracer.save()
